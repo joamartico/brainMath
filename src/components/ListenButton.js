13721881e12275ef11ebe5c09d4d1a7b7e5ac4f1
joamartico <joamartico@gmail.com>
2021-12-31 13:26:29 -0300

    spanish voice issue solved

--- src/components/ListenButton.js
+++ src/components/ListenButton.js
@@ -5,23 +5,26 @@ var voices = [];
 var synth = window.speechSynthesis;
 
 const ListenButton = ({ operation }) => {
-
-    function newOperationString (){
-        // replace "+" with " plus ", "-" with " minus ", "*" with " times ", "/" with " divided by "
-        return operation.replace(/-/g, " menos ").replace(/\*/g, " por ").replace(/\//g, " dividido ");
-    }
+  function newOperationString() {
+    // replace "+" with " plus ", "-" with " minus ", "*" with " times ", "/" with " divided by "
+    return operation.replace(/-/g, ' menos ').replace(/\*/g, ' por ').replace(/\//g, ' dividido ');
+  }
 
   async function onListen() {
     operation = await newOperationString(operation);
     voices = await synth.getVoices();
+
+    const mxVoice = voices.find((voice, i) => voice.name === 'Paulina');
+    const enVoice = voices.find((voice, i) => voice.name === 'Cecil');
+
     console.log(voices[8]);
     console.log('voices: ', voices);
     var utterThis = new SpeechSynthesisUtterance(operation);
-    utterThis.voice = voices[31];
+    utterThis.voice = mxVoice;
+    synth.speak(utterThis);
 
     // utterThis.pitch = operation;
     // utterThis.rate = rate.value;
-    synth.speak(utterThis);
   }
   return <Icon size={90} icon={volumeLow} onClick={onListen} />;
 };
